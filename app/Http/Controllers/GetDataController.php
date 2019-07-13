b171f89b87a1dc52255ea1ba18a114179eef8d7d
Miniplop <kevinj@theodo.fr>
2019-07-13 06:06:08 +0200

    add data on get Data

--- app/Http/Controllers/GetDataController.php
+++ app/Http/Controllers/GetDataController.php
@@ -13,7 +13,31 @@ class GetDataController extends Controller
     {
         if (file_exists("database.json")) {
             $response = file_get_contents("database.json");
-            return $response;
+
+            $data = json_decode($response, true);
+
+            $taskerMap = [];
+
+            foreach ($data['tasks'] as $task) {
+                $taskerId = $task['assignee_id'];
+                if (!array_key_exists($taskerId, $taskerMap)) {
+                    $taskerMap[$taskerId] = [
+                        'tasker_id' => $taskerId,
+                        'task_number' => 1,
+                        'first_task' => $task['dueTime'],
+                        'last_task' => $task['dueTime'],
+                        'working_time' => 30,
+                    ];
+                } else {
+                    $taskerMap[$taskerId]['task_number'] ++;
+                    $taskerMap[$taskerId]['last_task'] = $task['dueTime'];
+                    $taskerMap[$taskerId]['working_time'] += 30;
+                }
+            }
+
+            $data['plannings'] = array_values($taskerMap);
+
+            return $data;
         } else {
             return response()->json(json_decode('{}'));
         }
